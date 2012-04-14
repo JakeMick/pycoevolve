653bc9151ccb5cad2337b0bc3eb5671fc3b28dc9
Jake Mick <anon.biohacker@gmail.com>
2012-04-13 23:45:28 -0500

    PLZ HIRE ME

--- pycoevolve/seqs/tests/test_fasta.py
+++ pycoevolve/seqs/tests/test_fasta.py
@@ -11,10 +11,10 @@ def test_load_fasta_align():
     This tests load_fasta_align using toy_data's fastas.
     """
     aminobet = "-ACDEFGHIKLMNPQRSTVWY"
-    def ring_pop(str):
-        out = str[-1]
-        str = str[:-1]
-        return out + str
+    def ring_pop(amino_str):
+        out = amino_str[-1]
+        amino_str = amino_str[:-1]
+        return out + amino_str
 
 
     seq_loc = join(dirname(__file__),pardir)
@@ -27,7 +27,7 @@ def test_load_fasta_align():
     cyc_aln = load_fasta_align(cyc_loc)
     assert len(cyc_aln) == 210
     assert sum([int(seq_id) for seq_id in cyc_aln]) == \
-            sum([i for i in xrange(210)])
+            sum(xrange(210))
     for i in xrange(210):
         assert cyc_aln[str(i)] == aminobet
         aminobet = ring_pop(aminobet)
